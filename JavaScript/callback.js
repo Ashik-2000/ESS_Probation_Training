e24ebb0cc2dff54bf371c64a8b99fcb5b2379baf
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-09-01 19:15:25 +0600

    callback & recursion function

--- JavaScript/callback.js
+++ JavaScript/callback.js
@@ -1,10 +1,12 @@
-function display(x, y, result) {
-    console.log(`The addtion of ${x} & ${y} is: ${result}`);
-}
+// function display(x, y, result) {
+//     console.log(`The addtion of ${x} & ${y} is: ${result}`);
+// }
 
 function calculate(x, y, fun) {
     const result = x + y;
     fun(x, y, result);
 }
 
-calculate(10, 5, display);
+calculate(10, 5, (x, y, result) => {
+    console.log(`The addtion of ${x} & ${y} is: ${result}`);
+});
