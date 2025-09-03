058559ff4849ae3c13bcb0b941f77d40fe184c29
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-09-03 16:57:53 +0600

    loop complete

--- JavaScript/loop.js
+++ JavaScript/loop.js
@@ -8,19 +8,26 @@ console.log(text);
 
 const person = { fname: "John", lname: "Doe", age: 25 };
 const nums = [5, 9, 7, 3, 6, 8, 1, 2];
+
+// **********
 console.log("For in loop with object:");
 for (let x in person) {
     console.log(person[x]);
 }
+
+// **********
 console.log("For in loop with array:");
 for (let x in nums) {
     console.log(nums[x]);
 }
+
+// **********
 console.log("For of loop with array:");
 for (let x of nums) {
     console.log(x);
 }
 
+// **********
 console.log("for Each method in array:");
 nums.forEach((val, index, arr) => {
     console.log(val);
