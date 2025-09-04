2f29a1a79d02cbac1c704a07c5d76113c3aa4edb
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-09-04 12:16:26 +0600

    accessing nodes directly from grandparent node

--- JavaScript/dom.js
+++ JavaScript/dom.js
@@ -2,4 +2,7 @@ const grandpa = document.querySelector(".todo-list")
 const parent = grandpa.children;
 const childs = parent[1].children
 
+const childs2 = grandpa.querySelectorAll(".item");
+
 console.log(childs);
+console.log(childs2);
