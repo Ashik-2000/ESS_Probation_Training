57a1e02bff230774db6b24307fe05cb8a32da2fe
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-09-04 19:00:10 +0600

    recape complete

--- JavaScript/dom.js
+++ JavaScript/dom.js
@@ -1,15 +1,57 @@
-const newElement = document.createElement("div");
+// ***** Top to down Traversal *****
 
-newElement.className = "red";
+// const grandpa = document.querySelector(".todo-list")
+// const parent = grandpa.children;
+// const childs = parent[1].children
 
-newElement.setAttribute("id", "red");
+// const childs2 = grandpa.querySelectorAll(".item");
 
-console.log(newElement);
+// console.log(childs);
+// console.log(childs2);
 
-newElement.innerHTML = "Hello, Guys";
+// ***** Down to top traversal *****
 
-const container = document.querySelector(".todo-list");
-const firstElement = document.querySelector("#heading");
+// const child = document.querySelector(".item");
+// const parent = child.parentElement;
+// const grandpa = child.closest(".box");
+// console.log(parent);
+// console.log(grandpa);
 
-// container.insertBefore(newElement, firstElement)
-container.appendChild(newElement)
+// ***** Sibling traversal *****
+
+// const child = document.querySelector(".item");
+// const childNext = child.nextElementSibling;
+// const childPrev = childNext.previousElementSibling;
+
+// childNext.style.color = "red";
+// childPrev.style.color = "red";
+
+// ***** Manipulation *****
+
+// const newElement = document.createElement("div");
+
+// newElement.className = "red";
+
+// newElement.setAttribute("id", "red");
+
+// console.log(newElement);
+
+// newElement.innerHTML = "Hello, Guys";
+
+// const container = document.querySelector(".todo-list");
+// const firstElement = document.querySelector("#heading");
+
+// // container.insertBefore(newElement, firstElement)
+// container.appendChild(newElement);
+
+// ***** Listener *****
+
+const title = document.querySelector(".to-do");
+title.addEventListener("mouseenter", (event) => {
+    console.log(event);
+});
+
+// some most used events
+// click
+// input
+// submit
