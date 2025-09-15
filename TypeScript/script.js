62fa5d27d41bfa4aba11dd02076732e946ac7b30
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-09-15 19:47:14 +0600

    typescript setup and basic types and rules

---
+++ TypeScript/script.js
@@ -0,0 +1,5 @@
+var greetings = "Hello TypeScript";
+console.log(greetings.toLocaleUpperCase());
+var userID = 5222.25546;
+console.log(userID);
+// export {};
---
+++ TypeScript/script.ts
@@ -0,0 +1,5 @@
+var greetings: string = "Hello TypeScript";
+console.log(greetings.toLocaleUpperCase());
+var userID: number = 5222.25546;
+console.log(userID);
+// export {};
