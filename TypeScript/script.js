10639a11a979572b60ef87ad3bef0d4a138d864a
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-09-17 12:18:19 +0600

    unions recape

--- TypeScript/script.js
+++ TypeScript/script.js
@@ -1,8 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var myUser = {
-    id: 155,
-    name: "Ashik",
-    email: "ashik@gmail.com",
-    isActive: false
+var value;
+value = 50;
+value = "ashik";
+var ashik;
+ashik = {
+    username: "ashik",
+    id: 54,
 };
+function getDbId(id) {
+    if (typeof id === "string") {
+        id.toLowerCase();
+    }
+}
+getDbId("5");
+var data = [1, 2, 3];
+var data2 = ["1", "2", "3"];
+var data3 = [1, "2", 3];
--- TypeScript/script.ts
+++ TypeScript/script.ts
@@ -1,18 +1,33 @@
+let value: number | string;
+value = 50;
+value = "ashik";
+
 type User = {
-    readonly id: number,
-    name: string,
-    email: string,
-    isActive: boolean,
-    address?: string
+    name: string;
+    id: number;
 };
 
-const myUser: User = {
-    id: 155,
-    name: "Ashik",
-    email: "ashik@gmail.com",
-    isActive: false
+type Admin = {
+    username: string;
+    id: number;
+};
+
+let ashik: User | Admin;
+ashik = {
+    username: "ashik",
+    id: 54,
+};
+
+function getDbId(id: number | string) {
+    if (typeof id === "string") {
+        id.toLowerCase();
+    }
 }
 
+getDbId("5");
 
+const data: number[] = [1, 2, 3];
+const data2: string[] = ["1", "2", "3"];
+const data3: (string | number)[] = [1, "2", 3];
 
 export {};
