429484f8e290a8b7626b89bb1f52a946c7e7164f
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-09-29 20:01:02 +0600

    TypeScript & Async & Await

--- TypeScript/setUp/dist/index.js
+++ TypeScript/setUp/dist/index.js
@@ -1,17 +1,10 @@
-const updateAssignment = (assign, propsToUpdate) => {
-    return { ...assign, ...propsToUpdate };
-};
-const assign1 = {
-    studentID: "ab30",
-    title: "Mid Term",
-    grade: 45,
-};
-console.log(updateAssignment(assign1, { idPass: false }));
-const shapeList = {
-    circle: { radius: 5 },
-    Eclips: { radius: 5 },
-    square: { sideLength: 5 },
-    Rect: { sideLength: 5 },
-    triangle: { base: 5, height: 10 },
-};
+function getLength(item) {
+    return item.length;
+}
+getLength("Hello");
+getLength([1, 2, 3]);
+getLength({ length: 10 });
+// Correct usage
+let n = { length: 10 };
+getLength(n);
 export {};
--- TypeScript/setUp/src/index.ts
+++ TypeScript/setUp/src/index.ts
@@ -1,52 +1,15 @@
-interface Assignment {
-    studentID: string;
-    title: string;
-    grade: number;
-    idPass?: boolean;
+interface Length {
+    length: number;
 }
 
-type Shapes = "circle" | "triangle" | "square" | "Rect" | "Eclips";
-
-interface ShapeProperties {
-    radius?: number;
-    sideLength?: number;
-    base?: number;
-    height?: number;
+function getLength<T extends Length>(item: T): number {
+    return item.length;
 }
 
-const updateAssignment = (
-    assign: Assignment,
-    propsToUpdate: Partial<Assignment>
-) => {
-    return { ...assign, ...propsToUpdate };
-};
+getLength("Hello");
+getLength([1, 2, 3]);
+getLength({ length: 10 });
 
-const assign1: Readonly<Assignment> = {
-    studentID: "ab30",
-    title: "Mid Term",
-    grade: 45,
-};
-
-console.log(updateAssignment(assign1, { idPass: false }));
-
-const shapeList: Record<Shapes, ShapeProperties> = {
-    circle: { radius: 5 },
-    Eclips: { radius: 5 },
-    square: { sideLength: 5 },
-    Rect: { sideLength: 5 },
-    triangle: { base: 5, height: 10 },
-};
-
-interface ShapeList {
-    circle: { radius: 5 };
-    Eclips: { radius: 5 };
-    square: { sideLength: 5 }; 
-    Rect: { sideLength: 5 };
-    triangle: { base: 5; height: 10 };
-}
-
-type circularValue = Pick<ShapeList, "circle" | "Rect">;
-type circularShapes = Extract<Shapes, "circle" | "Eclips">
-type squareValue = Omit<ShapeList, "circle" | "Rect">;
-type sqaureShapes = Exclude<Shapes, "circle" | "Eclips">
-type namesOnly = NonNullable<Shapes>
+// Correct usage
+let n: Length = { length: 10 };
+getLength(n);
