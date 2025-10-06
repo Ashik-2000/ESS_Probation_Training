682f5828c793adb03d543d3c996aec960a03f8fd
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-10-06 15:14:57 +0600

    Started Angular

--- TypeScript/setUp/dist/index.js
+++ TypeScript/setUp/dist/index.js
@@ -1,10 +1,1 @@
-function getLength(item) {
-    return item.length;
-}
-getLength("Hello");
-getLength([1, 2, 3]);
-getLength({ length: 10 });
-// Correct usage
-let n = { length: 10 };
-getLength(n);
 export {};
