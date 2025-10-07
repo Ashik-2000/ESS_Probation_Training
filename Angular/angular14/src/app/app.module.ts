cd071e790e5f5816272109fe2963ff360c8a55b0
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-10-07 20:01:45 +0600

    section 18

--- Angular/angular14/src/app/app.module.ts
+++ Angular/angular14/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, BooksComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent], // lists all the components which should be knonw to Angular, at the point of time, it analyzes our index.html file
+  declarations: [
+    AppComponent,
+    BooksComponent,
+    ServerComponent,
+    ServersComponent,
+  ],
+  imports: [BrowserModule, FormsModule], // Gives all the functionality we need to start our app.
+  bootstrap: [AppComponent], // lists the component which can me mentioned in then index.html file and Angular Analyzes these at the starting of our project.
 })
 export class AppModule {}
