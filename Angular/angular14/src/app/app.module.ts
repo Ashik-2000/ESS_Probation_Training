6d74d81b67324bdb9366a2fe29ff5218f5e7d849
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-10-14 13:05:14 +0600

    basic and better directive

--- Angular/angular14/src/app/app.module.ts
+++ Angular/angular14/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './assignment2/assignment2.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { Assignment4Component } from './assignment4/assignment4.component';
+import { EvenComponent } from './assignment4/even/even.component';
+import { GameControlComponent } from './assignment4/game-control/game-control.component';
+import { OddComponent } from './assignment4/odd/odd.component';
+import { BasicHighlightDirective } from './directives-deep-dive/basic-highlight/basic-highlight.directive';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
-import { Assignment4Component } from './assignment4/assignment4.component';
-import { GameControlComponent } from './assignment4/game-control/game-control.component';
-import { EvenComponent } from './assignment4/even/even.component';
-import { OddComponent } from './assignment4/odd/odd.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { DirectivesDeepDiveComponent } from './directives-deep-dive/directives-deep-dive.component';
+import { BetterHighlightDirective } from './directives-deep-dive/better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -24,6 +27,9 @@ import { OddComponent } from './assignment4/odd/odd.component';
     GameControlComponent,
     EvenComponent,
     OddComponent,
+    BasicHighlightDirective,
+    DirectivesDeepDiveComponent,
+    BetterHighlightDirective,
   ],
   imports: [BrowserModule, FormsModule], // Gives all the functionality we need to start our app.
   bootstrap: [AppComponent], // lists the component which can me mentioned in then index.html file and Angular Analyzes these at the starting of our project.
---
+++ Angular/angular14/src/app/directives-deep-dive/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appBasicHighlight]',
+})
+export class BasicHighlightDirective implements OnInit {
+  constructor(private elementRef: ElementRef<HTMLElement>) {}
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+    this.elementRef.nativeElement.style.color = 'white';
+  }
+}
