a394ef283af2cae7246d72d59e4b990bad499d03
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-10-15 20:07:18 +0600

    service files created

--- Angular/udemyProject1/src/app/shared/dropdown.directive.ts
+++ Angular/udemyProject1/src/app/shared/dropdown.directive.ts
@@ -1,21 +1,21 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  // isOpen = false;
-  // constructor(
-  //   private elRef: ElementRef<HTMLElement>,
-  //   private renderer: Renderer2
-  // ) {}
-  @HostBinding('class.open') isOpen = false;
+  isOpen = false;
+  constructor(
+    private elRef: ElementRef<HTMLElement>,
+    private renderer: Renderer2
+  ) {}
+  // @HostBinding('class.open') isOpen = false;
   @HostListener('click') toogleOpen() {
     this.isOpen = !this.isOpen;
-    // if (this.isOpen) {
-    //   this.renderer.addClass(this.elRef.nativeElement, 'open');
-    // } else {
-    //   this.renderer.removeClass(this.elRef.nativeElement, 'open');
-    // }
+    if (this.isOpen) {
+      this.renderer.addClass(this.elRef.nativeElement, 'open');
+    } else {
+      this.renderer.removeClass(this.elRef.nativeElement, 'open');
+    }
   }
 }
