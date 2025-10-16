f5c968a7150b2507d5808a575b91d87b1f1e9bbd
Ashik Home <ashikulislamfb@gmail.com>
2025-10-16 08:44:52 +0600

    applied until 440

--- Angular/udemyProject1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/udemyProject1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,29 +8,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test 1',
-      'https://img.taste.com.au/usDoXvoa/taste/2018/01/healthy-chicken-chow-mein-134805-1.jpg'
-    ),
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test 2',
-      'https://img.taste.com.au/usDoXvoa/taste/2018/01/healthy-chicken-chow-mein-134805-1.jpg'
-    ),
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test 3',
-      'https://img.taste.com.au/usDoXvoa/taste/2018/01/healthy-chicken-chow-mein-134805-1.jpg'
-    ),
-  ];
-  constructor() {}
+  recipes: Recipe[] = [];
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit(): void {
+    this.recipeService.getRecipes();
   }
 }
--- Angular/udemyProject1/src/app/recipes/recipe.service.ts
+++ Angular/udemyProject1/src/app/recipes/recipe.service.ts
@@ -1,3 +1,28 @@
-export class RecipeService{
-  
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test 1',
+      'https://img.taste.com.au/usDoXvoa/taste/2018/01/healthy-chicken-chow-mein-134805-1.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test 2',
+      'https://img.taste.com.au/usDoXvoa/taste/2018/01/healthy-chicken-chow-mein-134805-1.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test 3',
+      'https://img.taste.com.au/usDoXvoa/taste/2018/01/healthy-chicken-chow-mein-134805-1.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
