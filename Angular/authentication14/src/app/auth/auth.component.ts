543cc912dd68438e67b1f6e2a9a52e65e5edcfbe
Ashik <ashikul.islam@efficientsoftwaresolutions.com>
2025-10-23 20:02:00 +0600

    Authentication LogIn SignUp

--- Angular/authentication14/src/app/auth/auth.component.ts
+++ Angular/authentication14/src/app/auth/auth.component.ts
@@ -1,7 +1,58 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLoginMode = true;
+  isLoading = false;
+  error: any = null;
+
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode(form: NgForm) {
+    this.isLoginMode = !this.isLoginMode;
+    form.reset();
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    this.isLoading = true;
+
+    // Creating a value to store a observable
+    let authObs: Observable<AuthResponseData>;
+
+    // Checking if user trying to login or logout
+    if (this.isLoginMode) {
+      // storing the logIn observale returned by service
+      authObs = this.authService.logIn(email, password);
+    } else {
+      // storing the signIn observale returned by service
+      authObs = this.authService.signUp(email, password);
+    }
+
+    // calling the subscribe() of the observables and dealing with errors
+    authObs.subscribe({
+      next: (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      error: (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      },
+    });
+
+    form.reset();
+  }
+}
--- Angular/authentication14/src/app/shared/data-storage.service.ts
+++ Angular/authentication14/src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { map, tap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
@@ -13,10 +13,10 @@ export class DataStorageService {
     const recipes = this.recipeService.getRecipes();
     this.http
       .put(
-        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json',
+        'https://authentication-tutorial-c876e-default-rtdb.firebaseio.com/recipes.json',
         recipes
       )
-      .subscribe(response => {
+      .subscribe((response) => {
         console.log(response);
       });
   }
@@ -24,20 +24,20 @@ export class DataStorageService {
   fetchRecipes() {
     return this.http
       .get<Recipe[]>(
-        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
+        'https://authentication-tutorial-c876e-default-rtdb.firebaseio.com/recipes.json'
       )
       .pipe(
-        map(recipes => {
-          return recipes.map(recipe => {
+        map((recipes) => {
+          return recipes.map((recipe) => {
             return {
               ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
             };
           });
         }),
-        tap(recipes => {
+        tap((recipes) => {
           this.recipeService.setRecipes(recipes);
         })
-      )
+      );
   }
 }
